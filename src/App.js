a57f4d3198021490e357c86ed187d7230fb4c08e
Jeffy Arias <jeff@Jeffys-MacBook-Pro.local>
2022-02-03 08:05:48 -0600

    changing app.js from aster2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey, Chamge from Master!</p>
+        <p>Hey, Chamge from Master WOW!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
